dd0fd33b5cbd60e2672c2e2a9e222d0ae47d0104
Bjorn Van Olmen <bjorn.vanolmen@boschrexroth.nl>
2022-08-05 08:58:40 +0200

    #1 MVVM Basic setup

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,19 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        // Make method to return data until data base is ready.
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from real database
+            yield return new Friend { Firstname = "Thomas", LastName = "Huber" };
+            yield return new Friend { Firstname = "Andreas", LastName = "Boehler" };
+            yield return new Friend { Firstname = "Julia", LastName = "Huber" };
+            yield return new Friend { Firstname = "Chrissi", LastName = "Egin" };
+        }
+
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+ 
+        public class ViewModelBase: INotifyPropertyChanged
+        {
+
+
+            // Interface of propertyChanged 
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            // Normal you would pass to OnPropertyChanged using Name 
+            // private void OnPropertyChanged(string propertyName )
+            // Called by :   OnPropertyChanged(nameof(SelectedFriend));
+            // Different approach is to make property optional. WHne this is adjusted it will at runtime automatically provide the name
+
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+
+        }
+    
+}
