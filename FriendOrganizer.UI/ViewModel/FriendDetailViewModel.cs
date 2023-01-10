f149cd26181be25e32f310f698ad385396dd13b3
BVO93 <bjorn.vanolmen@hotmail.com>
2023-01-10 13:41:43 +0100

    #8  Command interface implemented + update of GUI on save

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.Event;
+using Prism.Commands;
 using Prism.Events;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,28 @@ namespace FriendOrganizer.UI.ViewModel
 
             _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
                 .Subscribe(OnOpenFriendDetailView);
+
+            SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
+        }
+
+        private bool OnSaveCanExecute()
+        {
+            // Is runned at startup 
+
+            return true;
+           
+        }
+
+        private async  void OnSaveExecute()
+        {
+            await _dataService.SaveAsync(Friend);
+            _eventAggregator.GetEvent<AfterFriendSavedEvent>().Publish(
+                new AfterFriendSavedEventArgs
+                {
+                    Id = Friend.Id,
+                    DisplayMember = $"{Friend.Firstname} {Friend.LastName}"
+                });
+       
         }
 
         public async Task LoadAsync(int friendId)
@@ -40,12 +63,15 @@ namespace FriendOrganizer.UI.ViewModel
                 OnPropertyChanged();
             }
         }
-
-
-        public ICommand SaveCommand { get; }
+    
         private async void OnOpenFriendDetailView(int friendId) 
         {
             await LoadAsync(friendId);
         }
+
+        // COMMAND IMPLEMENT
+        public ICommand SaveCommand { get; }
+
+
     }
 }
--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -22,7 +22,8 @@ namespace FriendOrganizer.UI.ViewModel
         {
             _friendLookupService = friendLookupService;
             _eventAggregator = eventAggregator;
-            Friends = new ObservableCollection<LookupItem>();
+            Friends = new ObservableCollection<NavigationItemViewModel>();
+            _eventAggregator.GetEvent<AfterFriendSavedEvent>().Subscribe(AfterFriendSaved);
         }
 
         public async Task LoadAsync()
@@ -31,15 +32,15 @@ namespace FriendOrganizer.UI.ViewModel
             Friends.Clear();
             foreach (var item in lookup)
             {
-                Friends.Add(item);
+                Friends.Add(new NavigationItemViewModel(item.id, item.DisplayMember ));
             }
         }
 
-        public ObservableCollection<LookupItem> Friends { get; }
+        public ObservableCollection<NavigationItemViewModel> Friends { get; }
 
-        private LookupItem _selectedFriend;
+        private NavigationItemViewModel _selectedFriend;
 
-        public LookupItem SelectedFriend
+        public NavigationItemViewModel SelectedFriend
         {
             get { return _selectedFriend; }
             set
@@ -50,13 +51,18 @@ namespace FriendOrganizer.UI.ViewModel
                 if(_selectedFriend != null)
                 {
                     _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
-                        .Publish(_selectedFriend.id);
+                        .Publish(_selectedFriend.Id);
                 }
 
             }
 
         }
        
-
+        private void AfterFriendSaved(AfterFriendSavedEventArgs obj)
+        {
+            var LookupItem = Friends.Single(l => l.Id == obj.Id);
+            // Update screen tot the display member we receive from the event.
+            LookupItem.DisplayMember = obj.DisplayMember;
+        }
     }
 }
